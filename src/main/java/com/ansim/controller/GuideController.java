ecbd66bf13ae7e57934f2f3fe496d43d990284a7
20jin <20jin_lee@naver.com>
2023-12-26 15:44:22 +0900

    영진 푸시(서버쪽 네이버 로그아웃 수정)

--- src/main/java/com/ansim/controller/GuideController.java
+++ src/main/java/com/ansim/controller/GuideController.java
@@ -2,8 +2,10 @@ package com.ansim.controller;
 
 import com.ansim.service.GuideService;
 import com.ansim.util.Util;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +19,9 @@ public class GuideController {
     GuideService service;
 
     @GetMapping ("/guide/route")
-    public void getRoute() {}
+    public void getRoute(Model model, HttpSession session) {
+        model.addAttribute("accessToken", session.getAttribute("accessToken"));
+    }
 
     @ResponseBody
     @PostMapping  ("/guide/route")
